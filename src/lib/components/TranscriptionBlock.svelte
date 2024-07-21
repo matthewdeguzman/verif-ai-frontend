e55f9a9ee65c6fe741d8b236d98d39af708a179c
Jad Bendarkawi <jadb@princeton.edu>
2024-07-20 23:17:32 -0700

    latest

--- src/lib/components/TranscriptionBlock.svelte
+++ src/lib/components/TranscriptionBlock.svelte
@@ -12,19 +12,17 @@ export let transcript = "Transcript not loaded";
 </script>
 
 <div class="card bg-base-100 w-[584px] shadow-md border-2 border-[#D9D9D9]">
+  <span class="card-title pt-[50px] pb-[10px] pl-[30px] shadow-md">
+    <h2>Transcript</h2>
+    <span class="pt-[5px]">
+    <div class="badge border-0 text-[#D54F4F]"> <span class="pr-[3px]"><FalseIcon/></span><span>{falseCount} False</span></div>
+    <div class="badge border-0 text-[#7EAE72]"> <span class="pr-[3px]"><VerifiedIcon/></span><span>{verifiedCount} Verified</span></div>
+    <div class="badge border-0 text-[#F8C464]"> <span class="pr-[3px]"><Uncertainicon/></span><span>{uncertainCount} Uncertain</span></div>
+  </span>
+  </span>
+<div>
     <div class="card-body max-h-[610px] overflow-y-auto">
       <div class="top-0 right-0 absolute pt-[15px] pr-[30px]"><DownButton/>{" "}<UpButton/></div>
-
-        <span class="card-title pt-[15px]">
-          <h2>Transcript</h2>
-          <span class="pt-[5px]">
-          <div class="badge border-0 text-[#D54F4F]"> <span class="pr-[3px]"><FalseIcon/></span><span>{falseCount} False</span></div>
-          <div class="badge border-0 text-[#7EAE72]"> <span class="pr-[3px]"><VerifiedIcon/></span><span>{verifiedCount} Verified</span></div>
-          <div class="badge border-0 text-[#F8C464]"> <span class="pr-[3px]"><Uncertainicon/></span><span>{uncertainCount} Uncertain</span></div>
-        </span>
-        </span>
-      <div>
-
       <p class="text-[#757575]">{transcript}</p>
       </div>
     </div>
