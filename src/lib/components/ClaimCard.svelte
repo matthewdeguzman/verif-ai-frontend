462e911c9971c610a3700da7630c03b74ef6ca76
Jad Bendarkawi <jadb@princeton.edu>
2024-07-20 20:41:56 -0700

    timestamps

--- src/lib/components/ClaimCard.svelte
+++ src/lib/components/ClaimCard.svelte
@@ -1,36 +1,41 @@
 <script >
+  import { text } from "@sveltejs/kit";
+
 export let claimText;
 export let claimType;
-// export let timeStampStart;
-// export let timeStampEnd;
+// export let sources;
+export let timeStampStart;
+export let timeStampEnd;
 let textColor = "";
 let bgColor = "";
+let badgeColor = "";
 
 $: {
     if(claimType === "verified"){
         textColor = "7EAE72"
         bgColor = "bg-[#DFF3DA]"
+        badgeColor = "bg-[#7EAE72]"
     }
     else if (claimType === "false"){
         textColor = "D54F4F"
         bgColor = "bg-[#F8CECE]"
-
+        badgeColor = "bg-[#D54F4F]"
     }
     else{
         textColor = "F8C464"
         bgColor = "bg-[#FEF0D6]"
+        badgeColor = "bg-[#F8C464]"
     }
 }
 </script>
 
-<div class="card w-[584px] h-[200px] shadow-md border-2 border-[#{textColor}] {bgColor}">
+<div class="card w-[584px] h-[200px] shadow-md border-2 {bgColor}">
+    <div class="badge border-0 {badgeColor} text-white font-bold absolute right-0 top-0 m-3">{timeStampStart} : {timeStampEnd}</div>
   <div class="card-body">
-    <details class="relative">
-      <summary class="card-title text-[#{textColor}] cursor-pointer">
-        <h2 class="inline-block max-w-[35ch] overflow-hidden text-ellipsis whitespace-nowrap">
-          “{claimText}”
+      <div class="card-title text-[#{textColor}] cursor-pointer pt-[5px]">
+        <h2>
+          <span class="inline-block max-w-[35ch] overflow-hidden text-ellipsis whitespace-nowrap">{claimText}</span>
         </h2>
-       
-    </details>
-  </div>
+    </div>
+ </div>
 </div>
--- src/routes/transcription/+page.svelte
+++ src/routes/transcription/+page.svelte
@@ -9,7 +9,7 @@ import VideoPlayer from '$lib/components/VideoPlayer.svelte';
 		<div class="card bg-base-300 rounded-box grid min-h-[250px] place-items-center">
 			<VideoPlayer/>
 		</div>
-		<div class="rounded-box grid min-h-[250px] mt-[20px] place-items-center"><ClaimCard claimText={"20% of people are smelly"} claimType={"false"}/></div>
+		<div class="rounded-box grid min-h-[250px] mt-[15px] place-items-center"><ClaimCard claimText={"20% of people are smelly really long text in this claim"} claimType={"false"} timeStampStart={'0:31'} timeStampEnd={'0:50'}/></div>
 	  </div></div>
 	<div class="divider lg:divider-horizontal min-h-[500px]"></div>
 	<div class="card rounded-box grid h-32 place-items-center "><TranscriptionBlock falseCount={100} verifiedCount={100} uncertainCount={100} transcript={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit. At lectus urna duis convallis convallis tellus id interdum. Mi proin sed libero enim sed. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Porttitor rhoncus dolor purus non enim praesent elementum. Id leo in vitae turpis massa. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Quisque egestas diam in arcu. Quis lectus nulla at volutpat diam ut venenatis tellus in. Elementum integer enim neque volutpat ac. Euismod elementum nisi quis eleifend quam adipiscing vitae proin. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Vel turpis nunc eget lorem dolor sed viverra ipsum. Id venenatis a condimentum vitae sapien. In nulla posuere sollicitudin aliquam ultrices sagittis orci. \n "}/></div>
