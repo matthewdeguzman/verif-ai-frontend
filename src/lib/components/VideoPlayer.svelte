8436191f9d8a753b42b5f50562dbf3a4d5394a07
TeresaVu <teresavu353@gmail.com>
2024-07-20 20:13:42 -0700

    fix youtube link input and send the link to the VIdeoPlayer component

--- src/lib/components/VideoPlayer.svelte
+++ src/lib/components/VideoPlayer.svelte
@@ -1,5 +1,14 @@
 <script>
-export let url = "https://www.youtube.com/embed/TR-luawyows?si=nM4cR9AGYKQ0FRjs"
+    import { videoLink } from '../../stores.js'; // Adjust the path as needed
+    import { onMount } from 'svelte';
+
+    let url = '';
+
+    onMount(() => {
+        videoLink.subscribe(value => {
+        url = value;
+        });
+    });
 </script>
 
 <iframe  class="w-full h-[350px] rounded-box" src={url} title="YouTube video player" frameborder="0" allow="clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" ></iframe>
