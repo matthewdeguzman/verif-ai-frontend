c99d756f88de9f10f342d8a4024c1642a73297d9
Jad Bendarkawi <jadb@princeton.edu>
2024-07-20 18:37:28 -0700

    nav fix for now

--- src/lib/components/TranscriptionBlock.svelte
+++ src/lib/components/TranscriptionBlock.svelte
@@ -13,17 +13,16 @@ export let transcript = "Transcript not loaded";
 
 <div class="card bg-base-100 w-[584px] shadow-xl min-h-[500px]">
     <div class="card-body">
-        <span class="card-title">
+      <div class="top-0 right-0 absolute pt-[15px] pr-[30px]"><DownButton/>{" "}<UpButton/></div>
+
+        <span class="card-title pt-[15px]">
           <h2>Transcript</h2>
           <div class="badge border-0 text-[#D54F4F]"> <span class="pr-[3px]"><FalseIcon/></span><span>{falseCount} False</span></div>
           <div class="badge border-0 text-[#7EAE72]"> <span class="pr-[3px]"><VerifiedIcon/></span><span>{verifiedCount} Verified</span></div>
           <div class="badge border-0 text-[#D9D9D9]"> <span class="pr-[3px]"><Uncertainicon/></span><span>{uncertainCount} Uncertain</span></div>
-          <div class="pl-[20px]"><DownButton/>{" "}<UpButton/></div>
-        
-
         </span>
-    
       <div>
+
       <p class="text-[#757575]">{transcript}</p>
       </div>
     </div>
--- src/routes/transcription/+page.svelte
+++ src/routes/transcription/+page.svelte
@@ -5,7 +5,7 @@ import TranscriptionBlock from '$lib/components/TranscriptionBlock.svelte'
 <div class="flex w-full flex-col pt-10 pr-[100px] pl-[100px] lg:flex-row">
 	<div class="card bg-base-300 rounded-box grid flex-grow place-items-center h-[500px]">content</div>
 	<div class="divider lg:divider-horizontal "></div>
-	<div class="card rounded-box grid h-32 place-items-center "><TranscriptionBlock/></div>
+	<div class="card rounded-box grid h-32 place-items-center "><TranscriptionBlock falseCount={100} verifiedCount={100} uncertainCount={100}/></div>
   </div>
 
 
