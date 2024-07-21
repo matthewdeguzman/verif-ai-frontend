b77eaec347ca9547a4d5ee666ac749162061e64e
TeresaVu <teresavu353@gmail.com>
2024-07-20 17:35:53 -0700

    basic header and home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<div class="flex items-center justify-center min-h-screen bg-gray-100">
-	<h1 class="text-7xl font-bold text-center">
+<script>
+  
+</script>
+<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+	<p class="text-4xl text-center mt-2 font-light text-gray-500">
+		Fact check videos using AI
+	</p>
+	<h1 class="text-7xl mt-4 mb-4 font-bold text-center">
 	  Verif.ai
 	</h1>
+	<div class="mt-4 w-full max-w-md flex justify-center">
+		<input type="text" placeholder="Paste video link" class="input input-bordered w-full max-w-xs" />
+
+	  </div>
 </div>
